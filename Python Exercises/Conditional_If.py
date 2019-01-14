31d764db559ef1aaa22c533ec3f6ae5826214505
russelxrussel <vasquezjimrussel@gmail>
2019-01-14 10:47:42 +0800

    New Exercises 01142019

---
+++ Python Exercises/Conditional_If.py
@@ -0,0 +1,10 @@
+num = int(input("Enter Magic Number: "))
+
+if num == 8:
+	print("You got the Magic Number", num)
+elif num > 8:
+	print("Your input higher than magic number")
+else:
+	print("Input is low than magic number try again.")
+
+print("Please enter any key to exit...")
